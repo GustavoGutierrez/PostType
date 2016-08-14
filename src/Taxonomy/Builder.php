c1bb5d73535153a73fa74e806409a9ec216ae231
Gustavo Gutierrez <gustavogutierrezmercado@gmal.com>
2016-08-14 17:44:06 -0500

    Fix taxonomy

--- src/Taxonomy/Builder.php
+++ src/Taxonomy/Builder.php
@@ -8,12 +8,12 @@ namespace PostType\Taxonomy;
 class Builder {
 
 /**
- * Singular name for post type
+ * Singular name for taxonomy
  * @var string
  */
 	private $singular;
 	/**
-	 * Plural name for post type
+	 * Plural name for taxonomy
 	 * @var string
 	 */
 	private $plural;
@@ -22,13 +22,9 @@ class Builder {
 	 * @var string
 	 */
 	private $plural_end = 's';
+
 	/**
-	 * The labels for post type
-	 * @var array
-	 */
-	private $label;
-	/**
-	 * Default arguments for post type
+	 * Default arguments for taxonomy
 	 * @var array
 	 */
 	private $options = array(
@@ -69,7 +65,7 @@ class Builder {
 
 		$this->plural = strtolower($plural);
 
-        $this->post_types = $post_types;
+		$this->post_types = $post_types;
 
 		if ($options && is_array($options)) {
 			$this->options = array_merge($this->options, $options);
@@ -80,32 +76,32 @@ class Builder {
 		$this->register();
 	}
 
-    /**
-     * Facade
-     * @return void
-     */
-    public static function make($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL) {
-        new self($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL);
-    }
+	/**
+	 * Facade
+	 * @return void
+	 */
+	public static function make($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL) {
+		new self($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL);
+	}
 
-    /**
-     * Register the Taxonomy
-     * @return void
-     */
-    private function register() {
+	/**
+	 * Register the Taxonomy
+	 * @return void
+	 */
+	private function register() {
 
-        $this->options['labels'] = $this->label;
-        if (!isset($this->options['rewrite'])) {
-            $this->options['rewrite'] = array('slug' => $this->singular);
-        }
+		$this->options['labels'] = $this->label;
+		if (!isset($this->options['rewrite'])) {
+			$this->options['rewrite'] = array('slug' => $this->singular);
+		}
 
-        $_self = &$this;
+		$_self = &$this;
 
-        add_action('init', function () use (&$_self) {
-            register_taxonomy($_self->singular, $_self->post_types, $this->options);
-        });
+		add_action('init', function () use (&$_self) {
+			register_taxonomy($_self->singular, $_self->post_types, $this->options);
+		});
 
-    }
+	}
 
 	/**
 	 * Gets the Postypes by taxonomy.
@@ -138,104 +134,98 @@ class Builder {
 		return $this->label;
 	}
 
+	/**
+	 * Sets the Singular name for post type.
+	 *
+	 * @param string $singular the singular
+	 *
+	 * @return self
+	 */
+	private function _setSingular($singular) {
+		$this->singular = $singular;
 
-    /**
-     * Sets the Singular name for post type.
-     *
-     * @param string $singular the singular
-     *
-     * @return self
-     */
-    private function _setSingular($singular)
-    {
-        $this->singular = $singular;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Sets the Plural name for post type.
+	 *
+	 * @param string $plural the plural
+	 *
+	 * @return self
+	 */
+	private function _setPlural($plural) {
+		$this->plural = $plural;
 
-    /**
-     * Sets the Plural name for post type.
-     *
-     * @param string $plural the plural
-     *
-     * @return self
-     */
-    private function _setPlural($plural)
-    {
-        $this->plural = $plural;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Sets the The ending of plural.
+	 *
+	 * @param string $plural_end the plural end
+	 *
+	 * @return self
+	 */
+	private function _setPluralEnd($plural_end) {
+		$this->plural_end = $plural_end;
 
-    /**
-     * Sets the The ending of plural.
-     *
-     * @param string $plural_end the plural end
-     *
-     * @return self
-     */
-    private function _setPluralEnd($plural_end)
-    {
-        $this->plural_end = $plural_end;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Sets the The labels for post type.
+	 *
+	 * @param array $label the label
+	 *
+	 * @return self
+	 */
+	private function _setLabel(array $label) {
+		if ($label && is_array($label)) {
+			$this->label = $label;
+			return true;
+		}
 
-    /**
-     * Sets the The labels for post type.
-     *
-     * @param array $label the label
-     *
-     * @return self
-     */
-    private function _setLabel(array $label)
-    {
-        if ($label && is_array($label)) {
-            $this->label = $label;
-            return true;
-        }
+		$singular = $this->singular;
+		$plural = $this->plural ?: $singular . $this->plural_end;
 
-        $singular = $this->singular;
-        $plural = $this->plural ?: $singular . $this->plural_end;
+		$this->label = array(
+			'name' => ucfirst($plural),
+			'singular' => ucfirst($singular),
+			'menu_name' => ucfirst($singular),
+			'all_items' => __('All', 'wba_taxonomy') . ' ' . $plural,
+			'parent_item' => __('Parent', 'wba_taxonomy') . ' ' . $singular,
+			'parent_item_colon' => __('Parent', 'wba_taxonomy') . ' ' . $singular,
+			'new_item_name' => __('New', 'wba_taxonomy') . ' ' . $singular,
+			'add_new_item' => __('Add New', 'wba_taxonomy') . ' ' . $singular,
+			'edit_item' => __('Edit', 'wba_taxonomy') . ' ' . $singular,
+			'update_item' => __('Update', 'wba_taxonomy') . ' ' . $singular,
+			'view_item' => __('View', 'wba_taxonomy') . ' ' . $singular,
+			'separate_items_with_commas' => __('Separate', 'wba_taxonomy') . strtolower($singular) . ' ' . __('with commas', 'wba_taxonomy'),
+			'add_or_remove_items' => __('Add or remove', 'wba_taxonomy') . strtolower($singular),
+			'choose_from_most_used' => __('Choose from the most used', 'wba_taxonomy'),
+			'popular_items' => ucfirst($plural),
+			'search_items' => __('Search', 'wba_taxonomy') . ' ' . $singular,
+			'not_found' => __('Not Found', 'wba_taxonomy'),
+			'no_terms' => __('No', 'wba_taxonomy') . ' ' . $singular,
+			'items_list' => ucfirst($singular) . ' ' . __('list', 'wba_taxonomy'),
+			'items_list_navigation' => ucfirst($singular) . ' ' . __('list navigation', 'wba_taxonomy'),
+		);
 
-        $this->label = array(
-            'name' => ucfirst($plural),
-            'singular' => ucfirst($singular),
-            'menu_name' => ucfirst($singular),
-            'all_items' => __('All', 'wba_taxonomy') . ' ' . $plural,
-            'parent_item' => __('Parent', 'wba_taxonomy') . ' ' . $singular,
-            'parent_item_colon' => __('Parent', 'wba_taxonomy') . ' ' . $singular,
-            'new_item_name' => __('New', 'wba_taxonomy') . ' ' . $singular,
-            'add_new_item' => __('Add New', 'wba_taxonomy') . ' ' . $singular,
-            'edit_item' => __('Edit', 'wba_taxonomy') . ' ' . $singular,
-            'update_item' => __('Update', 'wba_taxonomy') . ' ' . $singular,
-            'view_item' => __('View', 'wba_taxonomy') . ' ' . $singular,
-            'separate_items_with_commas' => __('Separate', 'wba_taxonomy') . strtolower($singular) . ' ' . __('with commas', 'wba_taxonomy'),
-            'add_or_remove_items' => __('Add or remove', 'wba_taxonomy') . strtolower($singular),
-            'choose_from_most_used' => __('Choose from the most used', 'wba_taxonomy'),
-            'popular_items' => ucfirst($plural),
-            'search_items' => __('Search', 'wba_taxonomy') . ' ' . $singular,
-            'not_found' => __('Not Found', 'wba_taxonomy'),
-            'no_terms' => __('No', 'wba_taxonomy') . ' ' . $singular,
-            'items_list' => ucfirst($singular) . ' ' . __('list', 'wba_taxonomy'),
-            'items_list_navigation'=> ucfirst($singular) . ' ' . __('list navigation', 'wba_taxonomy')
-        );
+		return true;
+	}
 
-        return true;
-    }
+	/**
+	 * Sets the Default arguments for post type.
+	 *
+	 * @param array $options the options
+	 *
+	 * @return self
+	 */
+	private function _setOptions(array $options) {
+		$this->options = $options;
 
-    /**
-     * Sets the Default arguments for post type.
-     *
-     * @param array $options the options
-     *
-     * @return self
-     */
-    private function _setOptions(array $options)
-    {
-        $this->options = $options;
-
-        return $this;
-    }
+		return $this;
+	}
 
 }
