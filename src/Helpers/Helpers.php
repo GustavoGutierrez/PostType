e9f6d94e68b1dbded45019a72355985267e293cb
Gustavo Gutierrez <gustavogutierrezmercado@gmal.com>
2016-08-14 17:24:32 -0500

    Taxonomy Class

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -8,13 +8,15 @@ if (!function_exists('lang_post_type_domain')) {
 		load_textdomain('wba_posttype', dirname(dirname(__FILE__)) . '/PostType/languages');
 		//load_theme_textdomain('wba_posttype', dirname(dirname(__FILE__)) . '/PostType/languages');
 		//load_plugin_textdomain('wba_posttype', false, dirname(dirname(__FILE__)) . '/PostType/languages');
+
+		load_textdomain('wba_taxonomy', dirname(dirname(__FILE__)) . '/Taxonomy/languages');
 	}
 }
 add_action('init', 'lang_post_type_domain', 0);
 
 if (!function_exists('posttype')) {
-	function posttype($args = array()) {
-		return new CustomPostType\PostType\Builder();
+	function posttype($singular, $plural = NULL, $options = NULL, $labels = NULL) {
+		return new CustomPostType\PostType\Builder($singular, $plural, $options, $labels);
 	}
 }
 
@@ -25,7 +27,7 @@ if (!function_exists('metabox')) {
 }
 
 if (!function_exists('taxonomy')) {
-	function taxonomy($args = array()) {
-		return new CustomPostType\Taxonomy\Builder();
+	function taxonomy($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL) {
+		return new CustomPostType\Taxonomy\Builder($post_types, $singular, $plural, $options, $labels);
 	}
 }
--- src/Taxonomy/Builder.php
+++ src/Taxonomy/Builder.php
@@ -7,11 +7,38 @@ namespace PostType\Taxonomy;
  */
 class Builder {
 
+/**
+ * Singular name for post type
+ * @var string
+ */
+	private $singular;
 	/**
-	 * Name taxonomy
+	 * Plural name for post type
 	 * @var string
 	 */
-	private $name = '';
+	private $plural;
+	/**
+	 * The ending of plural
+	 * @var string
+	 */
+	private $plural_end = 's';
+	/**
+	 * The labels for post type
+	 * @var array
+	 */
+	private $label;
+	/**
+	 * Default arguments for post type
+	 * @var array
+	 */
+	private $options = array(
+		'hierarchical' => true,
+		'public' => true,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'show_in_nav_menus' => true,
+		'show_tagcloud' => true,
+	);
 
 	/**
 	 * Postypes by taxonomy
@@ -23,33 +50,62 @@ class Builder {
 	 * Label by taxonomy
 	 * @var array
 	 */
-	private $labels = array();
-
-	function __construct() {
-
-	}
+	private $label = array();
 
 	/**
-	 * Gets the Name taxonomy.
-	 *
-	 * @return string
+	 * Contructor Builder Taxonomy
+	 * @param string    $singular
+	 * @param mixed     $plural
+	 * @param mixed     $options
+	 * @param mixed     $labels
 	 */
-	public function getName() {
-		return $this->name;
+	public function __construct($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL) {
+
+		if (!is_string($singular)) {
+			throw new \Exception("Invalid sigular name provided as first argument: " . $singular, 1);
+		}
+
+		$this->singular = strtolower($singular);
+
+		$this->plural = strtolower($plural);
+
+        $this->post_types = $post_types;
+
+		if ($options && is_array($options)) {
+			$this->options = array_merge($this->options, $options);
+		}
+
+		$this->_setLabel($labels);
+
+		$this->register();
 	}
 
-	/**
-	 * Sets the Name taxonomy.
-	 *
-	 * @param string $name the name
-	 *
-	 * @return self
-	 */
-	private function _setName($name) {
-		$this->name = $name;
+    /**
+     * Facade
+     * @return void
+     */
+    public static function make($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL) {
+        new self($post_types, $singular, $plural = NULL, $options = NULL, $labels = NULL);
+    }
 
-		return $this;
-	}
+    /**
+     * Register the Taxonomy
+     * @return void
+     */
+    private function register() {
+
+        $this->options['labels'] = $this->label;
+        if (!isset($this->options['rewrite'])) {
+            $this->options['rewrite'] = array('slug' => $this->singular);
+        }
+
+        $_self = &$this;
+
+        add_action('init', function () use (&$_self) {
+            register_taxonomy($_self->singular, $_self->post_types, $this->options);
+        });
+
+    }
 
 	/**
 	 * Gets the Postypes by taxonomy.
@@ -78,20 +134,108 @@ class Builder {
 	 *
 	 * @return array
 	 */
-	public function getLabels() {
-		return $this->labels;
+	public function getLabel() {
+		return $this->label;
 	}
 
-	/**
-	 * Sets the Label by taxonomy.
-	 *
-	 * @param array $labels the labels
-	 *
-	 * @return self
-	 */
-	private function _setLabels(array $labels) {
-		$this->labels = $labels;
 
-		return $this;
-	}
+    /**
+     * Sets the Singular name for post type.
+     *
+     * @param string $singular the singular
+     *
+     * @return self
+     */
+    private function _setSingular($singular)
+    {
+        $this->singular = $singular;
+
+        return $this;
+    }
+
+    /**
+     * Sets the Plural name for post type.
+     *
+     * @param string $plural the plural
+     *
+     * @return self
+     */
+    private function _setPlural($plural)
+    {
+        $this->plural = $plural;
+
+        return $this;
+    }
+
+    /**
+     * Sets the The ending of plural.
+     *
+     * @param string $plural_end the plural end
+     *
+     * @return self
+     */
+    private function _setPluralEnd($plural_end)
+    {
+        $this->plural_end = $plural_end;
+
+        return $this;
+    }
+
+    /**
+     * Sets the The labels for post type.
+     *
+     * @param array $label the label
+     *
+     * @return self
+     */
+    private function _setLabel(array $label)
+    {
+        if ($label && is_array($label)) {
+            $this->label = $label;
+            return true;
+        }
+
+        $singular = $this->singular;
+        $plural = $this->plural ?: $singular . $this->plural_end;
+
+        $this->label = array(
+            'name' => ucfirst($plural),
+            'singular' => ucfirst($singular),
+            'menu_name' => ucfirst($singular),
+            'all_items' => __('All', 'wba_taxonomy') . ' ' . $plural,
+            'parent_item' => __('Parent', 'wba_taxonomy') . ' ' . $singular,
+            'parent_item_colon' => __('Parent', 'wba_taxonomy') . ' ' . $singular,
+            'new_item_name' => __('New', 'wba_taxonomy') . ' ' . $singular,
+            'add_new_item' => __('Add New', 'wba_taxonomy') . ' ' . $singular,
+            'edit_item' => __('Edit', 'wba_taxonomy') . ' ' . $singular,
+            'update_item' => __('Update', 'wba_taxonomy') . ' ' . $singular,
+            'view_item' => __('View', 'wba_taxonomy') . ' ' . $singular,
+            'separate_items_with_commas' => __('Separate', 'wba_taxonomy') . strtolower($singular) . ' ' . __('with commas', 'wba_taxonomy'),
+            'add_or_remove_items' => __('Add or remove', 'wba_taxonomy') . strtolower($singular),
+            'choose_from_most_used' => __('Choose from the most used', 'wba_taxonomy'),
+            'popular_items' => ucfirst(($plural),
+            'search_items' => __('Search', 'wba_taxonomy') . ' ' . $singular,
+            'not_found' => __('Not Found', 'wba_taxonomy'),
+            'no_terms' => __('No', 'wba_taxonomy') . ' ' . $singular,
+            'items_list' => ucfirst(($singular) . ' ' . __('list', 'wba_taxonomy'),
+            'items_list_navigation' => ucfirst(($singular) . ' ' . __('list navigation', 'wba_taxonomy'),
+        );
+
+        return true;
+    }
+
+    /**
+     * Sets the Default arguments for post type.
+     *
+     * @param array $options the options
+     *
+     * @return self
+     */
+    private function _setOptions(array $options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
 }
